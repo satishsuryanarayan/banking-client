798e27228c2f6523d179f3b9527542d5bfb23243
satishsuryanarayan <satsur@hotmail.com>
2025-06-22 23:42:46 -0500

    Print response

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,9 @@
+import json
 import random
 from datetime import datetime, timedelta
 
 from locust import HttpUser, task, between
+from requests.models import Response
 
 
 class Banker(HttpUser):
@@ -18,11 +20,21 @@ class Banker(HttpUser):
         to_account = random.choice(self.accounts)
         transfer = {"transfer": {"from_account_id": from_account, "to_account_id": to_account,
                                  "amount": round(random.uniform(1.00, 5000.00), 2)}}
-        self.client.post("/transfers", json=transfer, auth=(self.username, self.password))
+        with self.client.post("/transfers", json=transfer, auth=(self.username, self.password)) as response:
+            print(json.dumps(response.json(), indent=4))
 
     @task(1)
     def get_transfers(self):
         account_id = random.choice(self.accounts)
-        from_time = (datetime.now() + timedelta(hours=5)).isoformat()
+        from_time = datetime.now().isoformat()
         to_time = (datetime.now() + timedelta(hours=7)).isoformat()
-        self.client.get(f"/transfers/account/{account_id}?from_time={from_time}&to_time={to_time}", name = "/transfers/account", auth=(self.username, self.password), stream=True)
+        with self.client.get(f"/transfers/account/{account_id}?from_time={from_time}&to_time={to_time}", name = "/transfers/account", auth=(self.username, self.password), stream=True) as response:
+            for line in response.iter_lines():
+                try:
+                    decoded_line = line.decode('utf-8')
+                    json_data = json.loads(decoded_line)
+                    print(json.dumps(json_data, indent=4))
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e} - Line: {line}")
+                except UnicodeDecodeError as e:
+                    print(f"Error decoding bytes: {e} - Line: {line}")
