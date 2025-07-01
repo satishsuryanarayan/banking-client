9e71256a6ad55c0ed6b78bf027b1df0ab59aebd3
satishsuryanarayan <satsur@hotmail.com>
2025-06-30 19:38:07 -0500

    Fix imports

--- dtos/views/usersviewdto.py
+++ dtos/views/usersviewdto.py
@@ -2,7 +2,7 @@ from typing import Self
 
 from pydantic import model_validator
 
-from banking.apps.bank.v1.dtos.views.basedto import BaseDTO
+from dtos.views.basedto import BaseDTO
 
 
 class UsersViewDTO(BaseDTO):
