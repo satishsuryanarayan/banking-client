0b3ee0e5c76c43ad5839b3aa0e9d1d6be667900a
satishsuryanarayan <satsur@hotmail.com>
2025-07-01 02:20:08 -0500

    Include file name containing class in module

--- dtos/views/accountsviewdto.py
+++ dtos/views/accountsviewdto.py
@@ -4,4 +4,4 @@ from dtos.views.basedto import BaseDTO
 class AccountsViewDTO(BaseDTO):
     def __init__(self, **data):
         super().__init__(**data)
-        self.view = "AccountsView"
+        self.view = "banking.apps.bank.v1.view.accountsview.AccountsView"
--- dtos/views/usersviewdto.py
+++ dtos/views/usersviewdto.py
@@ -4,4 +4,4 @@ from dtos.views.basedto import BaseDTO
 class UsersViewDTO(BaseDTO):
     def __init__(self, **data):
         super().__init__(**data)
-        self.view = "UsersView"
+        self.view = "banking.apps.bank.v1.view.usersview.UsersView"
